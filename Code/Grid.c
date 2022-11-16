c16f2efbf99156489fa0defc6e73a95debb2ef4a
athebyme <367932@edu.itmo.ru>
2022-11-16 23:44:30 +0300

    First test commit

---
+++ Code/Grid.c
@@ -0,0 +1,17 @@
+#include "../Headers/Grid.h"
+
+struct grid{
+    int width;
+    int height;
+    int ** grid;
+};
+
+struct new_grid{
+    int width;
+    int height;
+    int ** grid;
+};
+
+int nextGeneration(){
+    return 0;
+}
---
+++ Headers/Grid.h
@@ -0,0 +1,8 @@
+//
+// Created by Anton on 16.11.2022.
+//
+
+#ifndef GAME_OF_LIFE_GRID_H
+#define GAME_OF_LIFE_GRID_H
+
+#endif //GAME_OF_LIFE_GRID_H
