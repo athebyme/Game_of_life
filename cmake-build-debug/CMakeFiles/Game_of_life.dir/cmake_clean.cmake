cbac579bd616f45e18b0d75e0b4fe375ed2f16ad
athebyme <367932@edu.itmo.ru>
2022-11-24 01:24:48 +0300

    updated yaml file

---
+++ cmake-build-debug/CMakeFiles/Game_of_life.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game_of_life.dir/main.c.obj"
+  "CMakeFiles/Game_of_life.dir/main.c.obj.d"
+  "Game_of_life.exe"
+  "Game_of_life.exe.manifest"
+  "Game_of_life.pdb"
+  "libGame_of_life.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Game_of_life.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
